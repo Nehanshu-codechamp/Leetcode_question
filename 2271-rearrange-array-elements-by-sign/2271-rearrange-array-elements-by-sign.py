293b8289f88522057b3d0433d265b8855d6d945b
Nehanshu shukla <122609193+Nehanshu-codechamp@users.noreply.github.com>
2025-10-14 14:58:46 +0530

    Time: 50 ms (50.14%), Space: 42.1 MB (74.63%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.py
@@ -1,14 +1,29 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        positive = []
-        negative =[]
-        for j in range (0,len(nums)):
-            if nums[j]>0:
-                positive.append(nums[j])
-            else:
-                 negative.append(nums[j])        
+        # positive = []
+        # negative =[]
+        # for j in range (0,len(nums)):
+        #     if nums[j]>0:
+        #         positive.append(nums[j])
+        #     else:
+        #          negative.append(nums[j])        
 
-        for i in range (0,len(positive)):
-            nums[2*i]=positive[i]
-            nums[2*i+1]=negative[i]
-        return nums    
+        # for i in range (0,len(positive)):
+        #     nums[2*i]=positive[i]
+        #     nums[2*i+1]=negative[i]
+        # return nums    
+# brute force solution hai first
+           
+           n = len(nums)
+           result =[0]*n
+           posIndex ,negIndex =0,1
+           
+           for i in range(0,n):
+            if nums[i]>=0:
+               result[posIndex] = nums[i]
+               posIndex+=2
+            else:
+                 result[negIndex] = nums[i]
+                 negIndex+=2
+
+           return result 
