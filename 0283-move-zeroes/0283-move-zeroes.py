ffb3d299c4c425fd64fcab3b6425f49c9d8f2783
Nehanshu shukla <122609193+Nehanshu-codechamp@users.noreply.github.com>
2025-10-23 18:39:44 +0530

    Time: 0 ms (100%), Space: 18.8 MB (79.6%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,17 +3,38 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n =len(nums)
-        temp = []
+        # n =len(nums)
+        # temp = []
 
-        for i in range (0,n):
-         if nums[i] != 0:
-            temp.append(nums[i])
+        # for i in range (0,n):
+        #  if nums[i] != 0:
+        #     temp.append(nums[i])
     
-        nz = len(temp)
-        for i in range (0,nz):
-          nums[i] = temp[i]
+        # nz = len(temp)
+        # for i in range (0,nz):
+        #   nums[i] = temp[i]
 
-        for i in range(nz,n):
-         nums[i]=0    
+        # for i in range(nz,n):
+        #  nums[i]=0    
           
+        n = len(nums)
+
+        if n == 1:
+            return
+        i=0
+        while i<n:
+            if nums[i] == 0:
+               break
+            i+=1
+
+        if i == n:
+            return
+            
+        j = i +1
+        while j <n:
+            if nums[j] != 0:
+                nums[i],nums[j] = nums[j],nums[i]
+                i +=1
+            j+=1        
+
+             
